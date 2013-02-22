c8f281fc0f95c06a6ff3e4212ff7d30bf073ef75
Chris J. Davis <cjd@chrisjdavis.org>
2013-02-22 13:13:44 -0600

    Split up the logic, and make the parser smarter
    We now inspect the content object returned and see if it is an object containing many objects, or a single object.

--- exporter.plugin.php
+++ exporter.plugin.php
@@ -2,32 +2,63 @@
 namespace Habari;
 class Exporter extends Plugin
 {
-  public static function parse(Array $args) {
+	private static function save($filename, $export_dir, $folder, $file) {
+		try {
+			$fp = fopen( Site::get_path('user') . '/' . $export_dir . '/' . $folder . '/' . $filename, 'w' );
+			if( $fp == false ) {
+				$fp = fopen( Site::get_path('user') . '/' . $export_dir . '/' . $folder . '/' . $filename, 'w' );
+			} else {
+				fwrite($fp, $file);
+				fclose($fp);
+			}
+		} catch( Exception $e ) {
+			echo $e->getMessage();
+			exit();
+		}
+
+	}
+	
+	public static function parse(Array $args) {
 		$objects = $args['objects'];
 		$template_dir = $args['template_location'];
 		$export_dir = $args['export_location'];
 		$templates = $args['template_types'];
 
+		Common::create_dir( Site::get_path('user') . '/' . $export_dir . '/' . $args['export_name'] );
+		
 		foreach( $templates as $template ) {
 			$file = file_get_contents( $template_dir . '/' . $template . '.html' );
-			
-			if( is_array( $objects[$template]['content'] ) ) {
-				foreach( $objects[$template]['content'] as $post ) {
+			$contents = $objects[$template]['content'];
+			if( $contents instanceof Posts ) {
+				foreach( $contents as $post ) {
 					foreach($objects[$template]['fields'] as $field ) {
-						$file = str_replace( "{" . $field . "}", $post->$field, $temp );
+						$file = str_replace( "{" . $field . "}", $post->$field, $file );
 					}
+					
+					if( $post->slug == $args['export_name'] ) {
+						$filename = 'index.html';
+					} else {
+						$filename = $post->slug . '.html';
+					}
+				
+					self::save( $filename, $export_dir, $args['export_name'], $file );
 				}
 			} else {
 				$post = $objects[$template]['content'];
+				
 				foreach($objects[$template]['fields'] as $field ) {
 					$file = str_replace( '{%' . $field . '%}', $post->$field, $file );
 				}
 			}
-						
-			$fp = fopen( Site::get_path('habari') . '/' . $export_dir . '/test/' . $post->slug, 'w' );
-			fwrite($fp, $file);
-			fclose($fp);
-		}		
+
+			if( $post->slug == $args['export_name'] ) {
+				$filename = 'index.html';
+			} else {
+				$filename = $post->slug . '.html';
+			}
+			
+			self::save( $filename, $export_dir, $args['export_name'], $file );
+		}
 	}
 }
 ?>
